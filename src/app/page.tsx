10bf19b29a2a5ac2b38a16c819b76f2d53472217
den.leonchev@gmal.com <den.leonchev@gmail.com>
2025-10-29 09:36:50 +0000

    Add content to the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,57 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Time to get started</h1>
-      <p>
-        <Link href="/meals">Meals</Link>
-      </p>
-      <p>
-        <Link href="/meals/share">Share Meal</Link>
-      </p>
-      <p>
-        <Link href="/community">Community</Link>
-      </p>
-    </main>
+    <>
+      <header className="">
+        <div className="max-w-1/2 my-5 ml-auto">
+          <h1 className="bg-linear-to-r from-amber-900 to-amber-500 bg-clip-text text-transparent text-3xl font-extrabold">
+            NextLevel Food for NextLevel Foodies
+          </h1>
+          <p className="mt-4">Taste & share food from all over the world</p>
+          <div className="flex space-x-3 mt-4">
+            <Link
+              className="p-2 text-amber-600 hover:underline"
+              href="/community"
+            >
+              Join the Community
+            </Link>
+            <Link
+              className="p-2 rounded-xl font-bold bg-linear-to-r from-amber-700 to-amber-400 hover:opacity-50 duration-200"
+              href="/meals"
+            >
+              Explore Meals
+            </Link>
+          </div>
+        </div>
+      </header>
+      <main className="mt-10">
+        <section>
+          <h2 className="my-6 font-extrabold text-center">How it works</h2>
+          <p className="my-4 text-center">
+            NextLevel Food is a platform for foodies to share their favorite
+            recepes with the world. It is a place to discover new dishes, and to
+            connect with other food lovers.
+          </p>
+          <p className="my-4 text-center">
+            NextLevel Food is a place to discover new dishes, and to conect with
+            other food lovers.
+          </p>
+        </section>
+        <section>
+          <h2 className="my-6 font-extrabold text-center">
+            Why NextLevel Food?
+          </h2>
+          <p className="my-4 text-center">
+            NextLevel Food is a platform for foodies to share their favorite
+            recepes with the world. It is a place to discover new dishes, and to
+            connect with other food lovers.
+          </p>
+          <p className="my-4 text-center">
+            NextLevel Food is a place to discover new dishes, and to conect with
+            other food lovers.
+          </p>
+        </section>
+      </main>
+    </>
   );
 }
