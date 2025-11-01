19253ba874bfc74215a5bc622ce340348c01b959
den.leonchev@gmal.com <den.leonchev@gmail.com>
2025-11-01 10:06:46 +0000

    Add active state to header navigation

--- src/components/main-header.tsx
+++ src/components/main-header.tsx
@@ -1,8 +1,22 @@
+"use client";
 import Link from "next/link";
 import logoImg from "@/assets/logo.png";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
+
+const navAnchors = [
+  {
+    url: "/meals",
+    text: "Browse Meals",
+  },
+  {
+    url: "/community",
+    text: "Foodies Community",
+  },
+] as const;
 
 export const MainHeader = () => {
+  const pathname = usePathname();
   return (
     <header className="flex flex-col md:flex-row md:justify-between px-4 md:px-8 font-bold">
       <Link
@@ -20,22 +34,26 @@ export const MainHeader = () => {
       </Link>
       <nav className="self-center my-2">
         <ul className="flex">
-          <li className="mx-4">
-            <Link
-              className="hover:drop-shadow-amber-900 hover:drop-shadow-xl hover:text-amber-500 duration-200"
-              href="/meals"
-            >
-              Browse Meals
-            </Link>
-          </li>
-          <li>
-            <Link
-              className="hover:drop-shadow-amber-900 hover:drop-shadow-xl hover:text-amber-500 duration-200"
-              href="/community"
-            >
-              Foodies Community
-            </Link>
-          </li>
+          {navAnchors.map((navAnchor) => {
+            const isActive = navAnchor.url === pathname;
+            const conditionalActiveClassnames = isActive
+              ? "drop-shadow-red-900 drop-shadow-lg text-amber-500"
+              : "";
+            const conditionalHoverClassnames = !isActive
+              ? "hover:underline"
+              : "";
+
+            return (
+              <li key={navAnchor.url} className="mx-4">
+                <Link
+                  className={`${conditionalHoverClassnames} ${conditionalActiveClassnames}`}
+                  href={navAnchor.url}
+                >
+                  {navAnchor.text}
+                </Link>
+              </li>
+            );
+          })}
         </ul>
       </nav>
     </header>
