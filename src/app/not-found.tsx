bee33919d7bbf50ed497a06e4581ddf63f12464a
den.leonchev@gmal.com <den.leonchev@gmail.com>
2025-11-06 08:43:34 +0000

    Add global "page not found" page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <main className="px-4 md:px-8">
+      <h1 className="mt-4 text-5xl uppercase text-center bg-linear-to-r from-amber-700 to-amber-400 bg-clip-text text-transparent">
+        404 Page was not found
+      </h1>
+      <p className="mt-2 text-center">Please go home page</p>
+    </main>
+  );
+}
