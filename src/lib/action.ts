62f662b975506db0a61b5c70263535f7ba59cd5d
den.leonchev@gmal.com <den.leonchev@gmail.com>
2025-11-11 08:57:10 +0000

    Add server side validation of save meal action

--- src/lib/action.ts
+++ src/lib/action.ts
@@ -1,17 +1,37 @@
 "use server";
 
 import { redirect } from "next/navigation";
+import { z } from "zod";
 import { saveMeal } from "./meals";
 
+const nonemptyString = z
+  .string()
+  .transform((t) => t?.trim())
+  .pipe(z.string().min(1));
+
+const nonemptyFile = z.instanceof(File).refine((file: File) => file.size > 0, {
+  message: "The file is empty",
+});
+
+const saveMealPayloadSchema = z.object({
+  title: nonemptyString,
+  summary: nonemptyString,
+  instructions: nonemptyString,
+  image: nonemptyFile,
+  name: nonemptyString,
+  email: nonemptyString,
+});
+
 export async function createMealAction(formData: FormData) {
-  const createMealPayload = {
-    title: formData.get("title") as string,
-    summary: formData.get("summary") as string,
-    instructions: formData.get("instructions") as string,
-    image: formData.get("image") as File,
-    name: formData.get("name") as string,
-    email: formData.get("email") as string,
-  };
+  const file = formData.get("image");
+  const createMealPayload = saveMealPayloadSchema.parse({
+    title: formData.get("title"),
+    summary: formData.get("summary"),
+    instructions: formData.get("instructions"),
+    image: formData.get("image"),
+    name: formData.get("name"),
+    email: formData.get("email"),
+  });
 
   await saveMeal(createMealPayload);
   redirect("/meals");
